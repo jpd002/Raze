dc05d2eab592415169126ea7f7e7003325e654d9
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2023-03-13 19:31:14 -0400

    Fix implementation of fetch_add.

--- source/common/platform/ps2/i_main.cpp
+++ source/common/platform/ps2/i_main.cpp
@@ -24,5 +24,7 @@ void UpdateVRModes(bool)
 extern "C"
 unsigned int __atomic_fetch_add_4(volatile void* mem, unsigned int val, int model)
 {
-	return *reinterpret_cast<volatile unsigned int*>(mem) + val;
+	unsigned int prevValue = *reinterpret_cast<volatile unsigned int*>(mem);
+	*reinterpret_cast<volatile unsigned int*>(mem) += val;
+	return prevValue;
 }
